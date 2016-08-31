c79716a41a9d34081eb3473cc9797ea0cfca36a7
Garrick Olson <garricko@stanford.edu>
2016-08-31 14:42:14 -0700

    Clean up standard ignore list

---
+++ src/main/java/com/github/susom/vertx/base/PortInfo.java
@@ -0,0 +1,13 @@
+package com.github.susom.vertx.base;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TODO: Add class description
+ *
+ * @author garricko
+ */
+public class PortInfo {
+  private static final Logger log = LoggerFactory.getLogger(PortInfo.class);
+}
